31d509b53dbcc43e71e04dc1d51c7e415ec0730d
Marnos Net <marcos@inaf27.com.ar>
2015-08-03 15:52:54 -0300

    Lista de Preguntas

--- models/models.js
+++ models/models.js
@@ -26,27 +26,22 @@ var sequelize = new Sequelize(DB_name, user, pwd,
   }      
 );
 
-/*// Usar BBDD SQLite:
-var sequelize = new Sequelize(null, null, null, 
-                       {dialect: "sqlite", storage: "quiz.sqlite"}
-                    );*/
+// Importar definicion de la tabla Quiz
+var quiz_path = path.join(__dirname,'quiz');
+var Quiz = sequelize.import(quiz_path);
 
+exports.Quiz = Quiz; // exportar tabla Quiz
 
-
-// Importar la definicion de la tabla Quiz en quiz.js
-var Quiz = sequelize.import(path.join(__dirname,'quiz'));
-
-exports.Quiz = Quiz; // exportar definición de tabla Quiz
-
-// sequelize.sync() crea e inicializa tabla de preguntas en DB
+// sequelize.sync() inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
-  // success(..) ejecuta el manejador una vez creada la tabla
+  // then(..) ejecuta el manejador una vez creada la tabla
   Quiz.count().then(function (count){
     if(count === 0) {   // la tabla se inicializa solo si está vacía
-      Quiz.create({ pregunta: 'Capital de Italia.',
-      	            respuesta: 'Roma'
-      	         })
-      .then(function(){console.log('Base de datos inicializada')});
+      Quiz.bulkCreate( 
+        [ {pregunta: 'Capital de Italia',   respuesta: 'Roma'},
+          {pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}
+        ]
+      ).then(function(){console.log('Base de datos inicializada')});
     };
   });
 });
